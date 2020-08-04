c9bb2b6cc5d83a19f1434181a8f4dbba3f1a073b
CarlosKarim <33940216+CarlosKarim@users.noreply.github.com>
2020-08-04 11:34:00 -0500

    ArchivoInicial

---
+++ ActivarGameOver.cs
@@ -0,0 +1,23 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mail;
+using UnityEngine;
+
+public class ActivarGameOver : MonoBehaviour
+{
+    public GameObject camaraGameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NotificationCenter.DefaultCenter().AddObserver(this, "PersonajeHaMuerto");
+    }
+
+    void PersonajeHaMuerto(Notification notificacion) {
+        camaraGameOver.SetActive(true);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
